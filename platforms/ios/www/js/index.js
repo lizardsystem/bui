fa57909c1ed7ac2ef1619b1e3465de6ccb408ce8
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-10-17 13:00:09 +0200

    Testing interaction using hammer.js

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -37,7 +37,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+/*        var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
@@ -45,5 +45,5 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+*/    }
 };
