cb9be66312e2f4901ccfdcfb822e31a05fbad847
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-11-18 14:01:33 +0100

    Adding check for iOS, resizing the slider bar height

--- www/js/script.js
+++ www/js/script.js
@@ -7,6 +7,11 @@
                 console.debug("Running as cordova application.\nWMS is loaded from the filesystem.\n");
                 document.addEventListener("deviceready", whichImagesAreWeTalking, true);
                 document.addEventListener("backbutton", onBackClickEvent, false);
+
+                if(/iOS/i.test(window.device.platform)) {
+                    console.debug('Running on iOS');
+                    document.getElementById('slider').style.height = '90px';
+                }
             }
             else {
                 console.debug("Running as web application.\nWMS is loaded from original wms source on the fly.\nCordova plugins behave unexpectedly, for debugging purposes only!\n");
